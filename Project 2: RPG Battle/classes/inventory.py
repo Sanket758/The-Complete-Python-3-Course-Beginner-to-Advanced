331d93d638377cf32f55dadbf248ef1d1850fe6c
Sanket Gadge <gadgesanket75@gmail.com>
2020-02-14 15:06:27 +0530

    Add files via upload

---
+++ Project 2: RPG Battle/classes/inventory.py
@@ -0,0 +1,8 @@
+
+
+class Item:
+    def __init__(self, name, type, description, prop):
+        self.name = name
+        self.type = type
+        self.description = description
+        self.prop = prop
